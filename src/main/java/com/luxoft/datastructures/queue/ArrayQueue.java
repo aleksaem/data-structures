2b89890496530806911bfd80dac8bf322e76ab41
Sasha <ilymx.xs@gmail.com>
2021-10-27 16:11:35 +0300

    First commit

--- src/main/java/com/luxoft/datastructures/queue/ArrayQueue.java
+++ src/main/java/com/luxoft/datastructures/queue/ArrayQueue.java
@@ -1,38 +1,74 @@
 package com.luxoft.datastructures.queue;
 
 public class ArrayQueue implements Queue{
+    private int size;
+    private int index;
+    private Object[] array;
+    public ArrayQueue() {
+        array = new Object[10];
+    }
+
+    public ArrayQueue(int initialCapacity) {
+        array = new Object[initialCapacity];
+    }
+
     @Override
     public void enqueue(Object value) {
+        ensureCapacity();
 
+        array[size] = value;
+        size++;
+    }
+
+    private void ensureCapacity() {
+        if(array.length == size){
+            Object[] newArray = new Object[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
     }
 
     @Override
     public Object dequeue() {
-        return null;
+        if(isEmpty()){
+            throw new IllegalStateException("Queue is empty!");
+        }
+        Object result = array[index];
+        index++;
+        size--;
+        return result;
     }
 
     @Override
     public Object peek() {
-        return null;
+        return array[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object value) {
+        for (int i = 0; i < size; i++) {
+            Object valueInQueue = array[i];
+            if(value.equals(valueInQueue)){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public void clear() {
-
+        size = 0;
     }
 }
--- src/test/java/com/luxoft/datastructures/queue/ArrayQueueTest.java
+++ src/test/java/com/luxoft/datastructures/queue/ArrayQueueTest.java
@@ -1,4 +1,108 @@
 package com.luxoft.datastructures.queue;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ArrayQueueTest {
+
+    @Test
+    public void testEnqueueAndDequeueWorksCorrectlyAndChangeSize(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("A");
+        arrayQueue.enqueue("B");
+
+        assertEquals(2, arrayQueue.size());
+        assertEquals("A", arrayQueue.dequeue());
+        assertEquals("B", arrayQueue.dequeue());
+        assertEquals(0, arrayQueue.size());
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testEnqueueOverInitialCapacityAndDequeueWorksCorrectlyAndChangeSize(){
+        ArrayQueue arrayQueue = new ArrayQueue(2);
+        arrayQueue.enqueue("A");
+        arrayQueue.enqueue("B");
+        arrayQueue.enqueue("C");
+
+        assertEquals(3, arrayQueue.size());
+        assertEquals("A", arrayQueue.dequeue());
+        assertEquals("B", arrayQueue.dequeue());
+        assertEquals("C", arrayQueue.dequeue());
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testEnqueueAndPeek(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("A");
+        arrayQueue.enqueue("B");
+
+
+        assertEquals(2, arrayQueue.size());
+        assertEquals("A", arrayQueue.peek());
+        assertEquals("A", arrayQueue.peek());
+        assertEquals(2, arrayQueue.size());
+    }
+
+    @Test
+    public void testIsEmptyReturnTrueOnNewQueue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyReturnFalseOnQueueWithData(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("A");
+
+        assertFalse(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyReturnTrueOnQueueAfterClear(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("A");
+        arrayQueue.enqueue("B");
+
+        arrayQueue.clear();
+
+        assertTrue(arrayQueue.isEmpty());
+    }
+
+    @Test
+    public void testContainsReturnTrue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("A");
+        arrayQueue.enqueue("B");
+
+        assertTrue(arrayQueue.contains("A"));
+    }
+
+    @Test
+    public void testContainsReturnFalse(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        arrayQueue.enqueue("A");
+        arrayQueue.enqueue("B");
+
+        assertFalse(arrayQueue.contains("C"));
+    }
+
+    @Test
+    public void testContainsReturnFalseOnEmptyQueue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+
+        assertFalse(arrayQueue.contains("C"));
+    }
+
+    @Test
+    public void testThrowIllegalStateExceptionWhenPopOnEmptyQueue(){
+        ArrayQueue arrayQueue = new ArrayQueue();
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            arrayQueue.dequeue();
+        });
+    }
 }
